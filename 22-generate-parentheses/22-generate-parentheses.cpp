0f91db0d776b3cbfc4e4b1b00b3716d8c0c2eec3
Rishabh Gupta <3rishabh3@gmail.com>
2022-06-24 16:34:43 +0530

    Time: 8 ms (42.90%), Space: 13.9 MB (38.49%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> ans;
+    void solve(int open, int close, int n, string s){
+        if(s.length() == n*2){
+            ans.push_back(s);
+            return;
+        }
+        if(open < n)
+            solve(open+1, close, n, s+"(");
+        if(close < open)
+            solve(open, close+1, n, s+")");
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        solve(0, 0, n, "");
+        return ans;
+    }
+};
